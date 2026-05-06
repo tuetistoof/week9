9416fd5744be7f6c4b48da21cb408a971aa1d56f
tuetistoof <tweantiit@gmail.com>
2026-05-06 20:54:23 +0700

    ci: fix path to wk9/9.3/pom.xml

--- 9.3/src/test/java/AppTest.java
+++ 9.3/src/test/java/AppTest.java
@@ -2,18 +2,15 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import LoggerFactory = org.slf4j.LoggerFactory;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
   private static final Logger logger = LoggerFactory.getLogger(AppTest.class);
 
   @Test
-  void testSample() {
-    logger.info("==> Dang thực hiện chạy test tích hợp CI/CD...");
-
-    // Ban đầu để là ĐÚNG để kiểm tra hệ thống hoạt động ổn định
+  public void testSample() {
+    logger.info("==> Dang chay test tu dong tren GitHub Actions...");
     assertEquals(1, 1);
   }
 }
